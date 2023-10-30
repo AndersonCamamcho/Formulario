1d73d4ffd0a941da8f2febc9895c0c25ab2d3818
Anderson <arex2407@gmail..com>
2023-10-30 13:24:25 -0500

    Validación cantidad de personas a registrar correcta

--- formulario.py
+++ formulario.py
@@ -1,6 +1,15 @@
 import datetime
 
-formularios = int(input(" » Ingrese la cantidad de formularios que desea imprimir: "))
+while True:
+    try:
+        formularios = int(input(" » Ingrese aqui la cantidad de personas a registrar: "))
+        if formularios > 0:
+            break
+        else:
+            print("Entrada no valida, el numero proporcionado debe ser positivo.")
+
+    except ValueError:
+        print("Entrada no valida, el valor proporcionado debe ser un entero.")
 nombre = []
 edad = []
 fecha_expedicion = []
@@ -8,24 +17,23 @@ fecha_expedicion = []
 for i in range(1,formularios+1):
 
 
-# Inicio furmulario que obtiene el nombre del formulario:
-
-
+# Inicio ontención de nombre para el formulario:
 
     while True:
 
-        nombre_formulario = input(" » Por favor ingrese un nombre para el formulario, sin que pase de 20 caracteres: ")
+        nombre_formulario = input(" » Ingrese aqui un nombre para el formulario, no mayor a 20 caracteres: ")
         validar_nombre = len(nombre_formulario)
         if validar_nombre <= 20 and not nombre_formulario[0].isspace():
             nombre.append(nombre_formulario)
             break
         else:
-            print("Entrada incorrecta")
+            print("Entrada incorrecta, asegurese de que el primer caracter no sea un espacio")
 
 
 
 
-#Inicio formulario de edad
+# Inicio obtención de edad para el formulario:
+
     while True:
         try:
             edad_formulario = int(input(" » Ingrese aqui su edad: "))
@@ -35,13 +43,13 @@ for i in range(1,formularios+1):
             else:
                 print("Recuerde que la edad debe estar entre 18 y 100 años, por favor intente de nuevo.")
         except ValueError:
-            print("No esta ingresando numeros enteros, por favor intente de nuevo")
+            print("No esta ingresando numeros enteros, por favor intente de nuevo.")
 
 
 
-#Inicio formulario de fecha de expeidción
+# Inicio obtención de fecha de expeidción para el formulario:
 
-    fecha_ingresada = input(" » Ingrese la fecha de expedicion con el siguiente formato: dia/mes/año(completo):   ")
+    fecha_ingresada = input(" » Ingrese aqui la fecha de expedicion con el siguiente formato: dia/mes/año(completo):   ")
     fecha_formulario= datetime.datetime.strptime(fecha_ingresada, "%d/%m/%Y")
     if fecha_formulario < datetime.datetime.now():
         fecha_expedicion.append(fecha_formulario)
