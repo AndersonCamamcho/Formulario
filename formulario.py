eff0ece38a27aa33d6e9590285888c9b9b458057
Anderson <arex2407@gmail..com>
2023-10-30 12:47:38 -0500

    Validación del nombre formulario correcta

--- formulario.py
+++ formulario.py
@@ -1,6 +1,6 @@
 import datetime
 
-formularios = int(input("Ingrese la cantidad de formularios que desea imprimir: "))
+formularios = int(input(" » Ingrese la cantidad de formularios que desea imprimir: "))
 nombre = []
 edad = []
 fecha_expedicion = []
@@ -10,37 +10,43 @@ for i in range(1,formularios+1):
 
 # Inicio furmulario que obtiene el nombre del formulario:
 
-    nombre_formulario = input("Por favor ingrese un nombre para el formulario, sin que pase de 20 caracteres: ")
-    validar_nombre = len(nombre_formulario)
 
-    if validar_nombre <= 20:
-        nombre.append(nombre_formulario)
-    else:
-        print("Nombre demasiado largo, inicie de nuevo el proceso")
-        break
+
+    while True:
+
+        nombre_formulario = input(" » Por favor ingrese un nombre para el formulario, sin que pase de 20 caracteres: ")
+        validar_nombre = len(nombre_formulario)
+        if validar_nombre <= 20 and not nombre_formulario[0].isspace():
+            nombre.append(nombre_formulario)
+            break
+        else:
+            print("Entrada incorrecta")
+
+
 
 
 #Inicio formulario de edad
     while True:
-            try:
-                edad_formulario = int(input("Ingrese aqui su edad: "))
-                if 18 <= edad_formulario <= 100:
-                    edad.append(edad_formulario)
-                    break
-                else:
-                    print("Recuerde que la edad debe estar entre 18 y 100 años, por favor intente de nuevo.")
-            except ValueError:
-                print("No esta ingresando numeros enteros, por favor intente de nuevo: ")
+        try:
+            edad_formulario = int(input(" » Ingrese aqui su edad: "))
+            if 18 <= edad_formulario <= 100:
+                edad.append(edad_formulario)
+                break
+            else:
+                print("Recuerde que la edad debe estar entre 18 y 100 años, por favor intente de nuevo.")
+        except ValueError:
+            print("No esta ingresando numeros enteros, por favor intente de nuevo")
+
 
 
 #Inicio formulario de fecha de expeidción
 
-    fecha_ingresada = input("Ingrese la fecha de expedicion con el siguiente formato: dia-mes-año:   ")
+    fecha_ingresada = input(" » Ingrese la fecha de expedicion con el siguiente formato: dia/mes/año(completo):   ")
     fecha_formulario= datetime.datetime.strptime(fecha_ingresada, "%d/%m/%Y")
     if fecha_formulario < datetime.datetime.now():
         fecha_expedicion.append(fecha_formulario)
     else:
-        print("Formato incorrecto, inicie de nuevo el proceso")
+        print("Esta ingresando un formato incorrecto")
         break
 
 
