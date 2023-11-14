7b1c25f48548d220ee82345371be048f2aec3d6e
Anderson <arex2407@gmail..com>
2023-11-13 20:18:37 -0500

    Se corrije el error de al momento de solo dar enter en el nombre y tambien se cambiaron el idioma de todas las variables

--- formulario.py
+++ formulario.py
@@ -1,15 +1,15 @@
 import datetime
 
 # Variables:
-nombre = []
-edad = []
-fecha_expedicion = []
-fecha_actual = datetime.datetime.now()
+name = []
+age = []
+expedition_date = []
+current_date = datetime.datetime.now()
 
 while True:
     try:
-        formularios = int(input(" » Ingrese aqui la cantidad de personas a registrar: "))
-        if formularios > 0:
+        forms = int(input(" » Ingrese aqui la cantidad de personas a registrar: "))
+        if forms > 0:
             break
         else:
             print("Entrada no valida, el numero proporcionado debe ser positivo.")
@@ -18,7 +18,7 @@ while True:
         print("Entrada no valida, el valor proporcionado debe ser un entero.")
 
 
-for i in range(1,formularios+1):
+for i in range(1, forms + 1):
 
     if i > 1:
         print("     **********************  *  *  *  ****************************")
@@ -26,28 +26,29 @@ for i in range(1,formularios+1):
         print("     **********************  *  *  *  ****************************")
 
 
-# Inicio ontención de nombre para el formulario:
-
-    while True:
-
-        nombre_formulario = input(" » Ingrese aqui un nombre para el formulario, no mayor a 20 caracteres: ")
-        validar_nombre = len(nombre_formulario)
-        if validar_nombre <= 20 and not nombre_formulario[0].isspace():
-            nombre.append(nombre_formulario)
-            break
-        else:
-            print("Entrada incorrecta.")
-
-
-
-
-# Inicio obtención de edad para el formulario:
-
+# Home name intention for the form:
     while True:
         try:
-            edad_formulario = int(input(" » Ingrese aqui su edad: "))
-            if 18 <= edad_formulario <= 100:
-                edad.append(edad_formulario)
+            form_name = input(" » Ingrese aqui un nombre para el formulario, no mayor a 20 caracteres: ")
+            validate_name = len(form_name)
+            if validate_name <= 20 and not form_name[0].isspace():
+                name.append(form_name)
+                break
+            else:
+                print("Entrada incorrecta.")
+        except:
+            print("Entrada incorrecta")
+
+
+
+
+
+# Home obtaining age for the form:
+    while True:
+        try:
+            form_ege = int(input(" » Ingrese aqui su edad: "))
+            if 18 <= form_ege <= 100:
+                age.append(form_ege)
                 break
             else:
                 print("Recuerde que la edad debe estar entre 18 y 100 años, por favor intente de nuevo.")
@@ -56,15 +57,15 @@ for i in range(1,formularios+1):
 
 
 
-# Inicio obtención de fecha de expeidción para el formulario:
+# Start obtaining the issue date for the form:
     while True:
         try:
-            fecha_ingresada = input(" » Ingrese aqui la fecha de expedicion con el siguiente formato: dia/mes/año(completo):   ")
-            fecha_formulario= datetime.datetime.strptime(fecha_ingresada, "%d/%m/%Y")
+            date_entered = input(" » Ingrese aqui la fecha de expedicion con el siguiente formato: dia/mes/año(completo):   ")
+            form_date= datetime.datetime.strptime(date_entered, "%d/%m/%Y")
 
-            fecha_por_validar = fecha_actual.year - fecha_formulario.year
-            if 0 <= fecha_por_validar <= 100:
-                fecha_expedicion.append(fecha_formulario)
+            date_to_be_validated = current_date.year - form_date.year
+            if 0 <= date_to_be_validated <= 100:
+                expedition_date.append(form_date)
                 break
             else:
                 print("La fecha ingresada no debe ser mayor a 100, ni menor a 0 años, intente de nuevo.")
@@ -77,15 +78,15 @@ for i in range(1,formularios+1):
 
 
 
-    if i == formularios:
+    if i == forms:
 
-        for j in range(0,formularios):
+        for j in range(0, forms):
 
             print("")
             print("***********  *  *****  *  *****  *  **********")
-            print(nombre[j].upper())
-            print("   Edad : ", edad[j])
-            print("   fecha de expedición : ", fecha_expedicion[j].strftime("%d/%m/%Y"))
+            print(name[j].upper())
+            print("   Edad : ", age[j])
+            print("   fecha de expedición : ", expedition_date[j].strftime("%d/%m/%Y"))
             print("***********  *  *****  *  *****  *  **********")
             print("")
 
